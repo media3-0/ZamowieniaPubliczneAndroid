c9bd1b6255198bc25e182085d79581116194a474
Adrian P <adrian248@gmail.com>
2015-07-16 12:06:45 +0200

    Dodany Recycler
    ..

--- app/src/main/java/pl/media30/zamowieniapubliczne/MainActivityFragment.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/MainActivityFragment.java
@@ -2,14 +2,23 @@ package pl.media30.zamowieniapubliczne;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.marshalchen.ultimaterecyclerview.UltimateRecyclerView;
+
+import pl.media30.zamowieniapubliczne.Adapters.MyAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    UltimateRecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+    MyAdapter mAdapter;
 
     public MainActivityFragment() {
     }
@@ -18,5 +27,24 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mRecyclerView = (UltimateRecyclerView) getView().findViewById(R.id.ultimate_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(new String[]{"adrian","rafa" +
+                "³","andrzej"});
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
