660d977bf0effb50b44a5a82fed1dbf5bb621685
andrzej560 <andrzej560@wp.pl>
2015-07-17 13:36:04 +0200

    Zamowienia

--- app/src/main/java/pl/media30/zamowieniapubliczne/Models/Zamowienie.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/Models/Zamowienie.java
@@ -6,11 +6,231 @@ import com.google.gson.annotations.SerializedName;
  * Created by Adrian on 2015-07-16.
  */
 public class Zamowienie {
-    int global_id;
+
     Zamowienie data;
     @SerializedName("zamowienia_publiczne.kod_pocztowy_id")
-    public  String kod_pocztowy_id;
+    public String kod_pocztowy_id;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_id")
+    public String zamawiajacy_id;
+
+    @SerializedName("zamowienia_publiczne_zamawiajacy.id")
+    public String zamawiajacyId;
+
+    @SerializedName("zamowienia_publiczne.aukcja")
+    public String aukcja;
+
+    @SerializedName("zamowienia_publiczne.rodzaj_id")
+    public String rodzaj_id;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_ulica")
+    public String zamawiajacy_ulica;
+
+    @SerializedName("zamowienia_publiczne.dsz_www")
+    public String dsz_www;
+
+    @SerializedName("zamowienia_publiczne_typy.symbol")
+    public String typySymbol;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_nazwa")
+    public String zamawiajacy_nazwa;
+
+    @SerializedName("zamowienia_publiczne.wartosc_cena_max")
+    public int wartosc_cena_max;
+
+    @SerializedName("zamowienia_publiczne.pozycja")
+    public int pozycja;
+
+    @SerializedName("zamowienia_publiczne.sprawozdanie_lata_obrotowe")
+    public String sprawozdanie_lata_obrotowe;
+
+    @SerializedName("zamowienia_publiczne.zmiana_umowy")
+    public String zmiana_umowy;
+
+    @SerializedName("zamowienia_publiczne.dialog")
+    public String dialog;
+
+    @SerializedName("zamowienia_publiczne_tryby.id")
+    public String trybyId;
+
+    @SerializedName("zamowienia_publiczne.zaliczka")
+    public String zaliczka;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_nr_miesz")
+    public String zamawiajacy_nr_miesz;
+
+    @SerializedName("zamowienia_publiczne_typy.nazwa")
+    public String typyNazwa;
+
+    @SerializedName("zamowienia_publiczne.projekt_ue")
+    public String projekt_ue;
+
+    @SerializedName("zamowienia_publiczne.nazwa")
+    public String nazwa;
+
+    @SerializedName("zamowienia_publiczne.akcept")
+    public String akcept;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_wojewodztwo")
+    public String zamawiajacy_wojewodztwo;
+
+    @SerializedName("zamowienia_publiczne.powiat_id")
+    public String powiat_id;
+
+    @SerializedName("zamowienia_publiczne.sprawozdanie_calosc")
+    public String sprawozdanie_calosc;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_miejscowosc")
+    public String zamawiajacy_miejscowosc;
+
+    @SerializedName("zamowienia_publiczne.typ_id")
+    public String typ_id;
+
+    @SerializedName("zamowienia_publiczne_zamawiajacy.kod_pocztowy_id")
+    public String zamawiajacyKod_pocztowy_id;
+
+    @SerializedName("zamowienia_publiczne.kryterium_kod")
+    public String kryterium_kod;
+
+    @SerializedName("zamowienia_publiczne.status_id")
+    public String status_id;
+
+    @SerializedName("zamowienia_publiczne.wykonawca_str")
+    public String wykonawca_str;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_regon")
+    public String zamawiajacy_regon;
+
+    @SerializedName("zamowienia_publiczne.czas")
+    public String czas;
+
+    @SerializedName("zamowienia_publiczne.tryb_id")
+    public String tryb_id;
+
+    @SerializedName("zamowienia_publiczne.numer_biuletynu")
+    public int numer_biuletynu;
+
+    @SerializedName("zamowienia_publiczne.publikacja_obowiazkowa")
+    public String publikacja_obowiazkowa;
+
+    @SerializedName("zamowienia_publiczne.dyn_www")
+    public String dyn_www;
+
+    @SerializedName("zamowienia_publiczne.liczba_miesiecy")
+    public int liczba_miesiecy;
+
+    @SerializedName("zamowienia_publiczne.instytucja_id")
+    public String instytucja_id;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_email")
+    public String zamawiajacy_email;
+
+    @SerializedName("zamowienia_publiczne.ogloszenie_bzp")
+    public String ogloszenie_bzp;
+
+    @SerializedName("zamowienia_publiczne.oferty_czesciowe")
+    public String oferty_czesciowe;
+
+    @SerializedName("zamowienia_publiczne_rodzaje.id")
+    public String rodzajeId;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_rodzaj_inny")
+    public String zamawiajacy_rodzaj_inny;
+
+    @SerializedName("zamowienia_publiczne.termin")
+    public String termin;
+
+    @SerializedName("zamowienia_publiczne.wartosc_szacunkowa")
+    public int wartosc_szacunkowa;
+
+    @SerializedName("zamowienia_publiczne.numer_zamowienia")
+    public int numer_zamowienia;
+
+    @SerializedName("zamowienia_publiczne.uniewaznienie")
+    public String uniewaznienie;
+
+    @SerializedName("zamowienia_publiczne.wojewodztwo_id")
+    public String wojewodztwo_id;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_tel")
+    public String zamawiajacy_tel;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_fax")
+    public String zamawiajacy_fax;
+
+    @SerializedName("zamowienia_publiczne.liczba_wykonawcow")
+    public int liczba_wykonawcow;
+
     @SerializedName("zamowienia_publiczne.zamawiajacy_www")
-    public  String zamawiajacy_www;
+    public String zamawiajacy_www;
+
+    @SerializedName("zamowienia_publiczne_zamawiajacy.nazwa")
+    public String zamawiajacyNazwa;
+
+    @SerializedName("zamowienia_publiczne.liczba_dni_oferty")
+    public int liczba_dni_oferty;
+
+    @SerializedName("zamowienia_publiczne.data_publikacji")
+    public String data_publikacji;
+
+    @SerializedName("zamowienia_publiczne_tryby.nazwa")
+    public String trybyNazwa;
+
+    @SerializedName("zamowienia_publiczne.data_stop")
+    public String data_stop;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_nr_domu")
+    public String zamawiajacy_nr_domu;
+
+    @SerializedName("zamowienia_publiczne.gmina_id")
+    public String gmina_id;
+
+    @SerializedName("zamowienia_publiczne.zmiana_ogloszenia")
+    public String zmiana_ogloszenia;
+
+    @SerializedName("zamowienia_publiczne.uzupelniajace")
+    public String uzupelniajace;
+
+    @SerializedName("zamowienia_publiczne_rodzaje.nazwa")
+    public String rodzajeNazwa;
+
+    @SerializedName("zamowienia_publiczne.liczba_dni")
+    public String liczba_dni;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_kod_poczt")
+    public String zamawiajacy_kod_poczt;
+
+    @SerializedName("zamowienia_publiczne.id")
+    public String id;
+
+    @SerializedName("zamowienia_publiczne.data_publikacji_zamowienia")
+    public String data_publikacji_zamowienia;
+
+    @SerializedName("zamowienia_publiczne.wartosc_szacowana")
+    public int wartosc_szacowana;
+
+    @SerializedName("zamowienia_publiczne.wariant")
+    public String wariant;
+
+    @SerializedName("zamowienia_publiczne_typy.id")
+    public String typyId;
+
+    @SerializedName("zamowienia_publiczne.zamawiajacy_rodzaj")
+    public String zamawiajacy_rodzaj;
+
+    @SerializedName("zamowienia_publiczne.wartosc_cena_min")
+    public String wartosc_cena_min;
+
+    @SerializedName("zamowienia_publiczne.liczba_czesci")
+    public int liczba_czesci;
+
+    @SerializedName("zamowienia_publiczne.oferty_godz")
+    public String oferty_godz;
+
+    @SerializedName("zamowienia_publiczne.wartosc_cena")
+    public int wartosc_cena;
+
+    @SerializedName("zamowienia_publiczne.zamowienie_ue")
+    public String zamowienie_ue;
 
 }
