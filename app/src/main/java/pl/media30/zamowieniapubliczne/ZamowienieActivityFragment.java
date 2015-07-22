34fc113604c8466782be68bf83613f906e89076e
sitt62 <rafal.rypczynski@gmail.com>
2015-07-22 11:52:36 +0200

    Male poprawki do poprzedniej wersji layoutu

--- app/src/main/java/pl/media30/zamowieniapubliczne/ZamowienieActivityFragment.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/ZamowienieActivityFragment.java
@@ -24,15 +24,12 @@ public class ZamowienieActivityFragment extends Activity {
 
     public ZamowienieActivityFragment() {
     }
-
-
-
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.fragment_zamowienie);
 
-            TextView nazwa = (TextView) findViewById(R.id.textViewNazwa);
+            //TextView nazwa = (TextView) findViewById(R.id.textViewNazwa);
             TextView zamawiajacyNazwa = (TextView) findViewById(R.id.textViewZamawiajacyNazwa);
             TextView zamawiajacyMiejscowosc = (TextView) findViewById(R.id.textViewZamawiajacyMiejscowosc);
             TextView zamawiajacyUlica = (TextView) findViewById(R.id.textViewZamawiajacyUlica);
@@ -48,10 +45,10 @@ public class ZamowienieActivityFragment extends Activity {
             TextView zamawiajacyFax = (TextView) findViewById(R.id.textViewZamawiajacyFax);
             TextView zamawiajacyWWW = (TextView) findViewById(R.id.textViewZamawiajacyWWW);
 
-            Bundle przekazanedane = getIntent().getExtras();
-            String przekazanytekst = przekazanedane.getString("dane");
+            //Bundle przekazanedane = getIntent().getExtras();
+            //String przekazanytekst = przekazanedane.getString("dane");
 
-            nazwa.setText(przekazanytekst);
+            //nazwa.setText(przekazanytekst);
 
 //parsowanie obj
 
@@ -62,7 +59,7 @@ public class ZamowienieActivityFragment extends Activity {
             }
             DataObjectClass myObject = new Gson().fromJson(jsonMyObject, DataObjectClass.class);
 
-            nazwa.setText(myObject.dataClass.nazwa.toString());
+            //nazwa.setText(myObject.dataClass.nazwa.toString());
             zamawiajacyNazwa.setText(myObject.dataClass.zamawiajacy_nazwa.toString());
             zamawiajacyMiejscowosc.setText(myObject.dataClass.zamawiajacy_miejscowosc.toString());
             zamawiajacyUlica.setText(myObject.dataClass.zamawiajacy_ulica.toString());
