36e7c238c150f4cb26595e1e2aa642ac19051f79
Adrian P <adrian248@gmail.com>
2015-07-28 10:31:31 +0200

    Poprawa działania wykresów największych zamówień

--- app/src/main/java/pl/media30/zamowieniapubliczne/WykresyActivity.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/WykresyActivity.java
@@ -1,8 +1,10 @@
 package pl.media30.zamowieniapubliczne;
 
+import android.annotation.TargetApi;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,10 +39,13 @@ import static java.lang.Integer.parseInt;
 
 public class WykresyActivity extends ActionBarActivity {
 
+    boolean wczytajRaz;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         final Context context = this;
+        wczytajRaz = true;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wykresy);
@@ -48,46 +53,33 @@ public class WykresyActivity extends ActionBarActivity {
         final ArrayList<String> labels = new ArrayList<String>();
         final BarChart chart = new BarChart(context);
 
-        /*
-        entries.add(new BarEntry(4f, 0));
-        entries.add(new BarEntry(8f, 1));
-        entries.add(new BarEntry(6f, 2));
-        entries.add(new BarEntry(12f, 3));
-        entries.add(new BarEntry(18f, 4));
-        entries.add(new BarEntry(9f, 5));
-        */
-
-
         RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("https://api.mojepanstwo.pl/dane/").build();
         MojePanstwoService service = restAdapter.create(MojePanstwoService.class);
         final ProgressDialog dialog =
                 ProgressDialog.show(this, "Trwa wczytywanie danych", "Please Wait...");
         service.najwiekszeZamowienia(new Callback<BaseListClass>() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
             @Override
             public void success(BaseListClass baseListClass, Response response) {
                 final List<DataObjectClass> dataObjectList = baseListClass.searchClass.dataobjects;
 
-                entries.add(new BarEntry((float) (dataObjectList.get(0).dataClass.wartosc_cena), 0));
-                entries.add(new BarEntry((float) (dataObjectList.get(1).dataClass.wartosc_cena), 1));
-                entries.add(new BarEntry((float) (dataObjectList.get(2).dataClass.wartosc_cena), 2));
-                entries.add(new BarEntry((float) (dataObjectList.get(3).dataClass.wartosc_cena), 3));
-                entries.add(new BarEntry((float) (dataObjectList.get(4).dataClass.wartosc_cena), 4));
-                entries.add(new BarEntry((float) (dataObjectList.get(5).dataClass.wartosc_cena), 5));
-
+                for(int i=0;i<dataObjectList.size();i++){
+                    entries.add(new BarEntry((float) (dataObjectList.get(i).dataClass.wartosc_cena),i));
+                }
 
                 BarDataSet dataset = new BarDataSet(entries, "Najwieksze zamowienia");
-
-                labels.add(dataObjectList.get(0).dataClass.nazwa);
-                labels.add(dataObjectList.get(1).dataClass.nazwa);
-                labels.add(dataObjectList.get(2).dataClass.nazwa);
-                labels.add(dataObjectList.get(3).dataClass.nazwa);
-                labels.add(dataObjectList.get(4).dataClass.nazwa);
-                labels.add(dataObjectList.get(5).dataClass.nazwa);
+                for(int i=0;i<dataObjectList.size();i++) {
+                    //labels.add(dataObjectList.get(i).slug);
+                    labels.add(Integer.toString(i));
+                }
                 setContentView(chart);
 
                 BarData data = new BarData(labels, dataset);
                 chart.setData(data);
                 chart.setDescription("");
+                chart.setScaleMinima(5f, 1f);
+                chart.animate();
+                chart.animateXY(4,4);
 
                 chart.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -99,37 +91,36 @@ public class WykresyActivity extends ActionBarActivity {
                 chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
                     @Override
                     public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-                        String info = "Nazwa: "+ dataObjectList.get(0).dataClass.nazwa;
-Log.d("Nazwa prz: ", info);
+                        if(wczytajRaz==true){
+                            String info = "Nazwa: "+ dataObjectList.get(e.getXIndex()).dataClass.nazwa;
+                            Log.d("Nazwa prz: ", info);
 
-                        Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
-                        Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(context, ZamowienieActivityFragment.class);
-                        DataObjectClass dataObjectClass = dataObjectList.get(e.getXIndex());
-                        String objToStr = new Gson().toJson(dataObjectClass);
-                        Bundle objClass = new Bundle();
-                        objClass.putString("myObject", objToStr);
-                        intent.putExtras(objClass);
-                        startActivity(intent);
+                            Toast.makeText(getApplicationContext(), info+"", Toast.LENGTH_SHORT).show();
+                            //Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
+                            //  Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(context, ZamowienieActivityFragment.class);
+                            DataObjectClass dataObjectClass = dataObjectList.get(e.getXIndex());
+                            String objToStr = new Gson().toJson(dataObjectClass);
+                            Bundle objClass = new Bundle();
+                            objClass.putString("myObject", objToStr);
+                            intent.putExtras(objClass);
+                            startActivity(intent);
+                            wczytajRaz=false;
+                            chart.dispatchSetSelected(false);
 
+
+                        }
                     }
-
                     @Override
                     public void onNothingSelected() {
-
                     }
                 });
-
-
                 dialog.dismiss();
-
             }
-
             @Override
             public void failure(RetrofitError error) {
                 Log.d("ERROR", "ERROR w retroficie");
                 Log.d("Erroe to:", error.getMessage() + "");
-
             }
         });
 
@@ -143,6 +134,11 @@ Log.d("Nazwa prz: ", info);
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_wykresy, menu);
         return true;
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        wczytajRaz=true;
     }
 
     @Override
