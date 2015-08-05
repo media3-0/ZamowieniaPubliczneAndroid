3a6909757a572187dbed67e15fd9b0900dacae49
Adrian P <adrian248@gmail.com>
2015-08-05 10:32:04 +0200

    Poprawnie działa wyszukiwanie po największych przetargach w głównej aktywności
    -lekkie problemy z wydajnością (wykresy długo się ładują)

--- app/src/main/java/pl/media30/zamowieniapubliczne/MojePanstwoService.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/MojePanstwoService.java
@@ -3,6 +3,7 @@ package pl.media30.zamowieniapubliczne;
 import pl.media30.zamowieniapubliczne.Models.SingleElement.BaseClass;
 import pl.media30.zamowieniapubliczne.Models.DownloadList.BaseListClass;
 import retrofit.Callback;
+import retrofit.client.Response;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import retrofit.http.Query;
@@ -21,5 +22,5 @@ public interface MojePanstwoService {
     void listOrdersWithParameter(@Query("page") int page, @Query("conditions[zamowienia_publiczne.zamawiajacy_miejscowosc]") String miejscowosc, @Query("conditions[zamowienia_publiczne.zamawiajacy_wojewodztwo]") String wojewodztwo ,@Query("conditions[zamowienia_publiczne.zamawiajacy_kod_poczt]") String kod_pocztowy, @Query("conditions[zamowienia_publiczne.zamawiajacy_nazwa]") String nazwa, @Query("conditions[zamowienia_publiczne.zamawiajacy_regon]") String regon, @Query("conditions[zamowienia_publiczne.zamawiajacy_www]") String zamaw_www, @Query("conditions[zamowienia_publiczne.zamawiajacy_email]") String zamaw_email, @Query("q") String glowne_zapyt, Callback<BaseListClass> clb);
 
     @GET("/dataset/zamowienia_publiczne/search?order=zamowienia_publiczne.wartosc_cena%20desc")
-    void najwiekszeZamowienia(@Query("q") String glowne_zapyt, @Query("conditions[zamowienia_publiczne.zamawiajacy_miejscowosc]") String miejscowosc, @Query("conditions[zamowienia_publiczne.zamawiajacy_wojewodztwo]") String wojewodztwo ,@Query("conditions[zamowienia_publiczne.zamawiajacy_kod_poczt]") String kod_pocztowy, @Query("conditions[zamowienia_publiczne.zamawiajacy_nazwa]") String nazwa, @Query("conditions[zamowienia_publiczne.zamawiajacy_regon]") String regon, @Query("conditions[zamowienia_publiczne.zamawiajacy_www]") String zamaw_www, @Query("conditions[zamowienia_publiczne.zamawiajacy_email]") String zamaw_email, Callback<BaseListClass> cbx);
+    BaseListClass najwiekszeZamowienia(@Query("page") int page, @Query("q") String glowne_zapyt, @Query("conditions[zamowienia_publiczne.zamawiajacy_miejscowosc]") String miejscowosc, @Query("conditions[zamowienia_publiczne.zamawiajacy_wojewodztwo]") String wojewodztwo ,@Query("conditions[zamowienia_publiczne.zamawiajacy_kod_poczt]") String kod_pocztowy, @Query("conditions[zamowienia_publiczne.zamawiajacy_nazwa]") String nazwa, @Query("conditions[zamowienia_publiczne.zamawiajacy_regon]") String regon, @Query("conditions[zamowienia_publiczne.zamawiajacy_www]") String zamaw_www, @Query("conditions[zamowienia_publiczne.zamawiajacy_email]") String zamaw_email);
 }
--- app/src/main/java/pl/media30/zamowieniapubliczne/WykresyActivity.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/WykresyActivity.java
@@ -1,17 +1,14 @@
 package pl.media30.zamowieniapubliczne;
 
-import android.annotation.TargetApi;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
@@ -24,22 +21,19 @@ import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import pl.media30.zamowieniapubliczne.Models.DownloadList.BaseListClass;
 import pl.media30.zamowieniapubliczne.Models.DownloadList.DataObjectClass;
-import pl.media30.zamowieniapubliczne.Models.SingleElement.BaseClass;
-import retrofit.Callback;
 import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-
-import static java.lang.Integer.parseInt;
 
 
 public class WykresyActivity extends ActionBarActivity {
 
     boolean wczytajRaz;
+    List<DataObjectClass> dataObjectList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,90 +46,166 @@ public class WykresyActivity extends ActionBarActivity {
         final ArrayList<BarEntry> entries = new ArrayList<>();
         final ArrayList<String> labels = new ArrayList<String>();
         final BarChart chart = new BarChart(context);
-        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("https://api.mojepanstwo.pl/dane/").build();
-        MojePanstwoService service = restAdapter.create(MojePanstwoService.class);
+
+
         final ProgressDialog dialog =
                 ProgressDialog.show(this, "Trwa wczytywanie danych", "Zaczekaj na wczytanie danych...");
-         service.najwiekszeZamowienia(RepositoryClass.getInstance().getGlowneZapyt(), RepositoryClass.getInstance().getWyszukiwanieMiasta(), RepositoryClass.getInstance().getWyszukiwanieWojew(), RepositoryClass.getInstance().getWyszukiwanieKodowPoczt(), RepositoryClass.getInstance().getWyszukiwanieZamawNazwa(), RepositoryClass.getInstance().getWyszukiwanieZamawREGON(), RepositoryClass.getInstance().getWyszukiwanieZamawWWW(), RepositoryClass.getInstance().getWyszukiwanieZamawEmail(), new Callback<BaseListClass>() {
-            @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
-            @Override
-            public void success(BaseListClass baseListClass, Response response) {
-                final List<DataObjectClass> dataObjectList = baseListClass.searchClass.dataobjects;
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("https://api.mojepanstwo.pl/dane/").build();
+                MojePanstwoService service = restAdapter.create(MojePanstwoService.class);
+                BaseListClass baseListClass = service.najwiekszeZamowienia(1, RepositoryClass.getInstance().getGlowneZapyt(), RepositoryClass.getInstance().getWyszukiwanieMiasta(), RepositoryClass.getInstance().getWyszukiwanieWojew(), RepositoryClass.getInstance().getWyszukiwanieKodowPoczt(), RepositoryClass.getInstance().getWyszukiwanieZamawNazwa(), RepositoryClass.getInstance().getWyszukiwanieZamawREGON(), RepositoryClass.getInstance().getWyszukiwanieZamawWWW(), RepositoryClass.getInstance().getWyszukiwanieZamawEmail());
 
-                for(int i=0;i<dataObjectList.size();i++){
-                    entries.add(new BarEntry((float) (dataObjectList.get(i).dataClass.wartosc_cena),i));
+                if (RepositoryClass.getInstance().getGlowneZapyt() == null) {
+                    dataObjectList = baseListClass.searchClass.dataobjects;
+                } else {
+                    final int wszystko = baseListClass.searchClass.paginationClass.total;
+                    final int iloscElemNaStr = baseListClass.searchClass.paginationClass.count;
+                    Log.d("wsz/str", wszystko + " " + iloscElemNaStr);
+                    dataObjectList = baseListClass.searchClass.dataobjects;
+                    dataObjectList.clear();
+                    for (int i = 0; i < Math.ceil(wszystko / iloscElemNaStr); i++) {
+                        RestAdapter restAdapterX = new RestAdapter.Builder().setEndpoint("https://api.mojepanstwo.pl/dane/").build();
+                        MojePanstwoService serviceX = restAdapterX.create(MojePanstwoService.class);
+                        BaseListClass baseListClass1 = serviceX.najwiekszeZamowienia(i, RepositoryClass.getInstance().getGlowneZapyt(), RepositoryClass.getInstance().getWyszukiwanieMiasta(), RepositoryClass.getInstance().getWyszukiwanieWojew(), RepositoryClass.getInstance().getWyszukiwanieKodowPoczt(), RepositoryClass.getInstance().getWyszukiwanieZamawNazwa(), RepositoryClass.getInstance().getWyszukiwanieZamawREGON(), RepositoryClass.getInstance().getWyszukiwanieZamawWWW(), RepositoryClass.getInstance().getWyszukiwanieZamawEmail());
+                        dataObjectList.addAll(baseListClass1.searchClass.dataobjects);
+                    }
                 }
-
-                BarDataSet dataset = new BarDataSet(entries, "Najwieksze zamowienia");
-                for(int i=0;i<dataObjectList.size();i++) {
-                    //labels.add(dataObjectList.get(i).slug);
-                    labels.add(Integer.toString(i));
-                }
-                setContentView(chart);
-
-                BarData data = new BarData(labels, dataset);
-                chart.setData(data);
-                chart.setDescription("");
-                chart.setScaleMinima(5f, 1f);
-                chart.animate();
-                chart.animateXY(4,4);
-
-                chart.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Toast.makeText(getApplicationContext(),"Element: "+ chart.getId(), Toast.LENGTH_SHORT).show();
-
-                    }
-                });
-                chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-                    @Override
-                    public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
-                        if(wczytajRaz==true){
-                            String info = "Nazwa: "+ dataObjectList.get(e.getXIndex()).dataClass.nazwa;
-                            Log.d("Nazwa prz: ", info);
-
-                            Toast.makeText(getApplicationContext(), info+"", Toast.LENGTH_SHORT).show();
-                            //Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
-                            //  Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(context, ZamowienieActivityFragment.class);
-                            DataObjectClass dataObjectClass = dataObjectList.get(e.getXIndex());
-                            String objToStr = new Gson().toJson(dataObjectClass);
-                            Bundle objClass = new Bundle();
-                            objClass.putString("myObject", objToStr);
-                            intent.putExtras(objClass);
-                            startActivity(intent);
-                            wczytajRaz=false;
-                            chart.dispatchSetSelected(false);
-
-
-                        }
-                    }
-                    @Override
-                    public void onNothingSelected() {
-                    }
-                });
-                dialog.dismiss();
-            }
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("ERROR", "ERROR w retroficie");
-                dialog.dismiss();
-                Toast.makeText(getApplicationContext(),"Błąd. Sprawdź połączenie z internetem",Toast.LENGTH_SHORT).show();
-                Log.d("Erroe to:", error.getMessage() + "");
-
-
             }
         });
 
+        t1.start();
+        try
+        {
+            t1.join();
+        } catch (
+                InterruptedException e
+                )
+
+        {
+            e.printStackTrace();
+        }
+
+
+        Collections.sort(dataObjectList, new Comparator<DataObjectClass>()
+
+                {
+                    @Override
+                    public int compare(DataObjectClass lhs, DataObjectClass rhs) {
+                        return Double.compare(rhs.dataClass.wartosc_cena, lhs.dataClass.wartosc_cena);
+                    }
+                }
+
+        );
 
 
 
+
+        Log.d("teraz","fffff");
+        for(
+                int i = 0;
+                i<dataObjectList.size();i++)
+
+        {
+            entries.add(new BarEntry((float) (dataObjectList.get(i).dataClass.wartosc_cena), i));
+        }
+
+        BarDataSet dataset = new BarDataSet(entries, "Najwieksze zamowienia");
+        for(
+                int i = 0;
+                i<dataObjectList.size();i++)
+
+        {
+            //labels.add(dataObjectList.get(i).slug);
+            labels.add(Integer.toString(i));
+        }
+
+        setContentView(chart);
+
+        BarData data = new BarData(labels, dataset);
+        chart.setData(data);
+        chart.setDescription("");
+        chart.setScaleMinima(5f,1f);
+        chart.animateXY(4,4);
+
+        chart.setOnClickListener(new View.OnClickListener()
+
+                                 {
+
+                                     @Override
+                                     public void onClick (View v){
+                                         Collections.sort(dataObjectList, new Comparator<DataObjectClass>() {
+                                             @Override
+                                             public int compare(DataObjectClass lhs, DataObjectClass rhs) {
+                                                 return Double.compare(rhs.dataClass.wartosc_cena, lhs.dataClass.wartosc_cena);//lhs.dataClass.wartosc_cena.  compareToIgnoreCase(rhs.dataClass.wartosc_cena);
+                                             }
+                                         });
+                                         // Toast.makeText(getApplicationContext(),"Element: "+ chart.getId(), Toast.LENGTH_SHORT).show();
+                                         for (int i = 0; i < dataObjectList.size(); i++) {
+                                             entries.add(new BarEntry((float) (dataObjectList.get(i).dataClass.wartosc_cena), i));
+                                         }
+
+                                         BarDataSet dataset = new BarDataSet(entries, "Najwieksze zamowienia");
+                                         for (int i = 0; i < dataObjectList.size(); i++) {
+                                             //labels.add(dataObjectList.get(i).slug);
+                                             labels.add(Integer.toString(i));
+                                         }
+                                         setContentView(chart);
+
+                                         BarData data = new BarData(labels, dataset);
+                                         chart.setData(data);
+                                         chart.setDescription("");
+                                         chart.setScaleMinima(5f, 1f);
+                                         chart.animateXY(4, 4);
+                                     }
+                                 }
+
+        );
+        chart.setOnChartValueSelectedListener(new
+
+                                                      OnChartValueSelectedListener() {
+                                                          @Override
+                                                          public void onValueSelected (Entry e,int dataSetIndex, Highlight h){
+                                                              if (wczytajRaz == true) {
+                                                                  String info = "Nazwa: " + dataObjectList.get(e.getXIndex()).dataClass.nazwa;
+                                                                  Log.d("Nazwa prz: ", info);
+
+                                                                  Toast.makeText(getApplicationContext(), info + "", Toast.LENGTH_SHORT).show();
+                                                                  //Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
+                                                                  //  Toast.makeText(getApplicationContext(), "Element: " + e.getXIndex(), Toast.LENGTH_SHORT).show();
+                                                                  Intent intent = new Intent(context, ZamowienieActivityFragment.class);
+                                                                  DataObjectClass dataObjectClass = dataObjectList.get(e.getXIndex());
+                                                                  String objToStr = new Gson().toJson(dataObjectClass);
+                                                                  Bundle objClass = new Bundle();
+                                                                  objClass.putString("myObject", objToStr);
+                                                                  intent.putExtras(objClass);
+                                                                  startActivity(intent);
+                                                                  wczytajRaz = false;
+                                                                  chart.dispatchSetSelected(false);
+
+
+                                                              }
+                                                          }
+
+                                                          @Override
+                                                          public void onNothingSelected () {
+                                                          }
+                                                      }
+
+        );
+        dialog.dismiss();
     }
 
+
+
+
+
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu){
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wykresy, menu);
+        getMenuInflater().inflate(R.menu.menu_wykresy,menu);
         return true;
     }
     @Override
@@ -145,17 +215,19 @@ public class WykresyActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item){
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int id=item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id==R.id.action_settings){
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
