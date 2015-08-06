a6156090169ef1d5ec3bccb6d744ab946b255b65
andrzej560 <andrzej560@wp.pl>
2015-08-06 14:14:32 +0200

    Wywalone menu z wykresów

--- app/src/main/java/pl/media30/zamowieniapubliczne/WykresyActivity.java
+++ app/src/main/java/pl/media30/zamowieniapubliczne/WykresyActivity.java
@@ -152,17 +152,8 @@ public class WykresyActivity extends ActionBarActivity {
                                                   public void onNothingSelected() {
                                                   }
                                               }
-
         );
         dialog.dismiss();
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wykresy, menu);
-        return true;
     }
 
     @Override
@@ -170,21 +161,5 @@ public class WykresyActivity extends ActionBarActivity {
         super.onResume();
         wczytajRaz = true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 }
